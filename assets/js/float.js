8c845dc0587d0f2d017544d312637f93d542e09e
unknown <ashishuiux@gmail.com>
2017-09-13 13:24:38 -0700

    added landing page that leads to different domains

--- assets/js/float.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  //     function loop() {
-    //      $("#cloud_image").animate({top:'+=20'}, 2000);
-    //      $("#cloud_image").animate({top:'-=20'}, 2000, loop);
-    //  }
-    //
-    //  loop();
-
-})
